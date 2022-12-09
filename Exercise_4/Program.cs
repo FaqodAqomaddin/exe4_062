ff081ccba066397440a6b251b650832bc506997e
FaqodAqomaddin <faqod.aqomaddin.ft21@mail.umy.ac.id>
2022-12-09 16:17:10 +0700

    Add class and Method Push

--- Exercise_4/Program.cs
+++ Exercise_4/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using System;
+using System.Collections;
+
+namespace Exercise_4
+{
+    class Exercise_4
+    {
+        static void Main(string[] args)
+        {
+            Stack st = new Stack();
+            st.Push('w');
+            st.Push('x');
+            st.Push('y');
+            Console.WriteLine("Count of elements in the current Stack: " + st.Count);
+            Console.WriteLine("Stack = ");
+            foreach (char ch in st)
+            {
+                Console.WriteLine(ch + " ");
+            }
+        }
+    }
+}
